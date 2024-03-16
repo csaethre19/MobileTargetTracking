844791be529d3a14e3f9242e62104d99bf862e8d
KirraKotsenburg <kirrakotz@hotmail.com>
2024-03-16 16:03:50 -0600

    got rid of print statements clogging up tracking

--- src/Tracking.cpp
+++ src/Tracking.cpp
@@ -7,6 +7,7 @@ using namespace legacy;
 class LegacyTrackerWrapper : public cv::Tracker
 {
     const Ptr<legacy::Tracker> legacy_tracker_;
+    
 
 public:
     LegacyTrackerWrapper(const Ptr<legacy::Tracker> &legacy_tracker)
@@ -73,7 +74,9 @@ void Tracking::continuousTracking()
     // Read first frame
     cout << "Reading first frame..." << endl;
     Mat frame;
+
     bool ok = video.read(frame);
+    
     if (!ok)
     {
         cout << "ERROR: Could not read frame" << endl;
@@ -99,11 +102,9 @@ void Tracking::continuousTracking()
     // Resize frame
     resize(frame, frame, Size(frameWidth, frameHeight));
 
-    cout << "Initializing video writer..." << endl;
-    // NOT NEEDED - Initialize video writer
-    //VideoWriter output(trackerType + ".avi", VideoWriter::fourcc('X', 'V', 'I', 'D'), 60.0, Size(frameWidth / 2, frameHeight / 2), true);
-    
-    cout << "Selecting ROI manually with frame..." << endl;
+    // NOT NEEDED - Initialize video writer - creates a video
+    //VideoWriter output(trackerType + ".mp4", VideoWriter::fourcc('X', 'V', 'I', 'D'), 60.0, Size(frameWidth / 2, frameHeight / 2), true);
+
     // Select ROI
     Rect bbox = selectROI(frame, false);
 
@@ -112,8 +113,6 @@ void Tracking::continuousTracking()
     
     // Initialize tracker with first frame and bounding box
     tracker->init(frame, bbox); // TODO: call in constructor after getTracker()
-    cout << "Tracker initialized." << endl;
-    cout << "Tracking Started..." << endl;
 
     // TODO: extract the while loop outside Tracking (in Main.cpp)
     // We will do this so we can have continuousTracking() return p1 and p2
@@ -128,24 +127,21 @@ void Tracking::continuousTracking()
         // Start timer
         double timer = (double)getTickCount();
 
-        cout << "Updating tracker..." << endl;
+        //cout << "Updating tracker..." << endl;
 
         // Update tracking result
         bool found = tracker->update(frame, bbox);
-        cout << "Tracker updated!" << endl;
+       // cout << "Tracker updated!" << endl;
 
         // Calculate Frames per second (FPS)
         float fps = getTickFrequency() / ((double)getTickCount() - timer);
 
         if (found)
         {
-            cout << "Tracking Success!" << endl;
             // Tracking success: Draw the tracked object
             Point p1(cvRound(bbox.x), cvRound(bbox.y));                            // Top left corner
             Point p2(cvRound(bbox.x + bbox.width), cvRound(bbox.y + bbox.height)); // Bottom right corner
 
-            cout << "P1: (" << p1.x << ", " << p1.y << ")" << endl;
-            cout << "P2: (" << p2.x << ", " << p2.y << ")" << endl;
             
             rectangle(frame, p1, p2, Scalar(255, 0, 0), 2, 1);
         }
@@ -163,7 +159,7 @@ void Tracking::continuousTracking()
 
         // Display result
         imshow("Tracking", frame);
-        //output.write(frame);
+       // output.write(frame);
 
         // Exit if ESC pressed
         int k = waitKey(1);
@@ -173,10 +169,9 @@ void Tracking::continuousTracking()
         }
     }
 
-    cout << "Ending tracking" << endl;
 
     video.release();
-    //output.release();
+   // output.release();
 
     destroyAllWindows();
 }
