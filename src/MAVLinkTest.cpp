09cd3e29c7ee7529ce2a357893c5e94500d685ae
Charlotte Saethre <charlotte.saethre>
2024-07-18 23:09:48 +0100

    detailed comments - currently just passing through values no calculations are actually made

--- src/MAVLinkTest.cpp
+++ src/MAVLinkTest.cpp
@@ -3,30 +3,28 @@
 #include "Logger.h"
 
 int main() {
-    // create logger
+    // Create logger
     Logger appLogger("app_logger", "debug.log");
     auto logger = appLogger.getLogger();
 
-    // open uart
+    // Open uart
     UART uart(logger, "/dev/ttyS0");
     int uart_fd = uart.openUART();
 
-    // Hard-coded points
+    // Calculate center of bbox using hard-coded points
     Point p1(448, 261);
     Point p2(528, 381);
-    // Calculate center of bbox
     int xc = (p1.x + p2.x) / 2;
     int yc = (p1.y + p2.y) / 2;
     
-    // Hard-coded lat/lon
+    // Calculate distance and update lat/lon with starting hard-coded lat/lon values
     float curr_lat = 40.75;
     float curr_lon = -111.93;
-
     float distance = calculate_distance(xc, yc);
     float updated_lat = calculate_updated_lat(curr_lat, distance);
     float updated_lon = calculate_updated_lon(curr_lon, distance);
 
+    // Create gps MAVLink message and send over UART
     std::vector<uint8_t> gps_msg = create_gps_msg(updated_lat, updated_lon);
-    // int num_wrBytes = write(uart_fd, gps_msg, strlen(gps_msg)); // will this work?
     int num_wrBytes = write(uart_fd, gps_msg.data(), gps_msg.size());
 }
