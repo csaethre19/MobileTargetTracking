ffb823cc0a25733bd9ac8d6ac23588345b2d06ca
Charlotte Saethre <charlotte.saethre@gmail.com>
2024-07-19 20:03:07 -0600

    adding some calculations

--- src/MAVLinkTest.cpp
+++ src/MAVLinkTest.cpp
@@ -12,19 +12,20 @@ int main() {
     int uart_fd = uart.openUART();
 
     // Calculate center of bbox using hard-coded points
-    Point p1(448, 261);
-    Point p2(528, 381);
+    Point p1(100, 100);
+    Point p2(1000, 1000);
     int xc = (p1.x + p2.x) / 2;
     int yc = (p1.y + p2.y) / 2;
     
     // Calculate distance and update lat/lon with starting hard-coded lat/lon values
-    float curr_lat = 40.75;
-    float curr_lon = -111.93;
-    float distance = calculate_distance(xc, yc);
-    float updated_lat = calculate_updated_lat(curr_lat, distance);
-    float updated_lon = calculate_updated_lon(curr_lon, distance);
+    double curr_lat = 40.75;
+    double curr_lon = -111.93;
+    double distance = calculate_distance(xc, yc);
+    double updated_lat = calculate_updated_lat(curr_lat, distance);
+    double updated_lon = calculate_updated_lon(curr_lon, distance);
 
     // Create gps MAVLink message and send over UART
     std::vector<uint8_t> gps_msg = create_gps_msg(updated_lat, updated_lon);
+    parse_lat_lon(gps_msg); // should print 40.76140, -111.918598
     int num_wrBytes = write(uart_fd, gps_msg.data(), gps_msg.size());
 }
