6858993991f12ebd24938783907fdb7f0b60f96a
Charlotte Saethre <charlotte.saethre>
2024-07-22 21:01:12 +0100

    updates tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -4,7 +4,7 @@
 # To run: ./disable_serial_getty.sh
 sudo systemctl stop serial-getty@ttyS0.service
 sudo systemctl disable serial-getty@ttyS0.service
+sudo chmod 666 /dev/ttyS0 # for some reason I get permission issues now with accessing ttyS0
 cd build
-# cd Desktop/MobileTargetTracking/build
 make TestMain
 sudo ./TestMain
